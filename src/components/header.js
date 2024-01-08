80715b43529267e2ecf3d7dbd27cd7be033f562b
Dragonbestgg1 <dragonbestgg1@gmail.com>
2024-01-08 13:08:07 +0200

    It ka visas vajadzigas lapas

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from '../styles/header.module.css';
 
-function Header(){
+function Header(){{/*uz tokens bus*/}
   const navigate = useNavigate();
 
   const handleClick = (event) => {
@@ -15,7 +15,10 @@ function Header(){
       <div className={`${styles.header}`}>
         <a className={`${styles.res}`} href="/" onClick={handleClick}>Home</a>
         <a className={`${styles.res}`} href="/admin" onClick={handleClick}>Control Panel</a>
-        
+        <a className={`${styles.res}`} href="/data">Data</a>
+        <a className={`${styles.res}`} href="/orders">Orders</a>
+        <a className={`${styles.res}`} href="/reports">Reports</a>
+        <a className={`${styles.res}`} href="/storage">Storage</a>
         <a className={`${styles.res}`} href="/login" onClick={handleClick}>Login</a>
       </div>
     </div>
---
+++ src/orders/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "../styles/admin.module.css";
+import { useState } from "react";   
+import axios from 'axios';
+
+function Orders(){
+    return(
+        <div className={`${style.main}`}>
+
+        </div>
+    )
+}
+
+export default Orders;
