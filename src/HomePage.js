c02bdcea45ebc8875b42de85daa26a84f15303e3
Sintijaa <ipb21.s.martinsone@vtdt.edu.lv>
2024-01-09 12:33:14 +0200

    Labojumi

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 function HomePage() {
   return (
     <div>
-      <h1>Welcome to the Home Page!</h1>
+      <h1>Welcome!</h1>
       <img src="https://images.pexels.com/photos/3806690/pexels-photo-3806690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
     </div>
   );
