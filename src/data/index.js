fb28021103672dcbb736a35f9beedc8df035ff35
demo212121 <113087144+demo212121@users.noreply.github.com>
2024-01-09 13:45:46 +0200

    Frontend: Data;Reports
    Frontend: Data;Reports

--- src/data/index.js
+++ src/data/index.js
@@ -6,8 +6,26 @@ import axios from 'axios';
 function Data(){
     return(
         <div className={`${style.main}`}>
-            
+  <form id="productForm">
+    <h2>Preces informācija</h2>
+    <label for="productName">Preces nosaukums:</label>
+    <input type="text" id="productName" name="productName"/>
+
+    <label for="productCategory">Preces kategorija:</label>
+    <select id="productCategory" name="productCategory">
+      <option value="electronics">Elektronika</option>
+      <option value="clothing">Apģērbs</option>
+      <option value="home">Mājas preces</option>
+    </select>
+
+    <label for="productPrice">Preces cena:</label>
+    <input type="text" id="productPrice" name="productPrice"/>
+
+    <label for="productDescription">Preces apraksts:</label>
+    <textarea id="productDescription" name="productDescription" rows="4" cols="50"></textarea>
+
+    <input type="submit" value="Pievienot preces informāciju"/>
+  </form>
         </div>
     )
-}
-export default Data;
+} export default Data;
--- src/reports/index.js
+++ src/reports/index.js
@@ -6,7 +6,31 @@ import axios from 'axios';
 function Reports(){{/*uz tokens bus*/}
     return(
         <div className={`${style.main}`}>
-
+             <h2>Noliktavas preču atskaite</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>Preces kods</th>
+        <th>Preces nosaukums</th>
+        <th>Preces daudzums</th>
+        <th>Kategorija</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>001</td>
+        <td>Dators</td>
+        <td>25</td>
+        <td>Elektronika</td>
+      </tr>
+      <tr>
+        <td>002</td>
+        <td>T-krekls</td>
+        <td>150</td>
+        <td>Apģērbs</td>
+      </tr>
+    </tbody>
+  </table>
         </div>
     )
 }
