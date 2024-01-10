48db0fc7f2cd60a2c7beca7bada18e7836edd42d
Sintijaa <ipb21.s.martinsone@vtdt.edu.lv>
2024-01-10 09:47:19 +0200

    Uzlaboju stilu

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,8 +5,9 @@ function HomePage() {
   return (
     <div className='main-container'>
       {/* <h1>Welcome to the Home Page!</h1> */}
-      <img src="https://images.pexels.com/photos/3806690/pexels-photo-3806690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-      <img src="https://images.pexels.com/photos/3806690/pexels-photo-3806690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+      <img src="https://images.pexels.com/photos/8532928/pexels-photo-8532928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+      <div class="bottom-left">Labrīt ;)</div>
+      {/* <img src="https://images.pexels.com/photos/3806690/pexels-photo-3806690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" /> */}
     </div>
   );
 }
